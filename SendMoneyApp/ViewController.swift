1a9c77dfa9fc6225ee8e3c3ee4f9a2e239253c41
DmitryP0l <dmitrypoletaeu@gmail.com>
2024-08-20 12:39:42 +0300

    Initial Commit

---
+++ SendMoneyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendMoneyApp
+//
+//  Created by Dmitry P on 20.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ SendMoneyAppUITests/SendMoneyAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SendMoneyAppUITestsLaunchTests.swift
+//  SendMoneyAppUITests
+//
+//  Created by Dmitry P on 20.08.24.
+//
+
+import XCTest
+
+final class SendMoneyAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
