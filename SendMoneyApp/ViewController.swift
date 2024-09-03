c0a09f5dc3300eb58547a9d0fc74b0b720d1c473
DmitryP0l <dmitrypoletaeu@gmail.com>
2024-09-03 17:23:46 +0300

    commit
    render first display.
    Use AutoLayout for setup elements.

--- SendMoneyApp/ViewController.swift
+++ SendMoneyApp/ViewController.swift
@@ -8,13 +8,134 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	// MARK: - Constants
+	
+	private var startingViewContainer: UIView = {
+		let view = UIView()
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.backgroundColor = .lightGray
+		return view
+	}()
+	
+	private var welcomeLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Welcome to Send Money App"
+		label.textAlignment = .left
+		label.textColor = .white
+		label.font = label.font.withSize(60)
+		label.numberOfLines = 0
+		return label
+	}()
+	
+	private var informationLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Transfer money that is easier."
+		label.textAlignment = .left
+		label.textColor = .white
+		label.font = label.font.withSize(28)
+		label.numberOfLines = 0
+		return label
+	}()
+	
+	private var startButton: UIButton = {
+		let button = UIButton(type: .system)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.backgroundColor = .white
+		button.setTitle("Start Banking", for: .normal)
+		button.setTitleColor(.darkGray, for: .normal)
+		button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+		button.layer.cornerRadius = 12
+		return button
+	}()
+	
+	// MARK: - Lifecycle
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = .red
+		setupScreen()
 		
 	}
-
-
+	
+	// MARK: - Private methods
+	///  Метод настройки всего экрана
+	private func setupScreen() {
+		
+		view.backgroundColor = .darkGray
+		setupWelcomeLabel()
+		setupStartingViewContainer()
+		setupInformationLabel()
+		setupStartButton()
+	}
+	/// Настройка ограничений (constrains) для welcomeLabel
+	private func setupWelcomeLabel() {
+		
+		view.addSubview(welcomeLabel)
+		
+		NSLayoutConstraint.activate([
+			welcomeLabel.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: 20),
+			welcomeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+			welcomeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+		])
+	}
+	
+	/// Настройка ограничений (constrains) для startingViewContainer
+	private func setupStartingViewContainer() {
+		
+		view.addSubview(startingViewContainer)
+		
+		NSLayoutConstraint.activate([
+			startingViewContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			startingViewContainer.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+			startingViewContainer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+			startingViewContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4)
+		])
+		
+		startingViewContainer.layer.cornerRadius = 60
+		startingViewContainer.layer.maskedCorners = [.layerMaxXMinYCorner]
+	}
+	
+	/// Настройка ограницений (constrains) для informationLabel
+	private func setupInformationLabel() {
+		
+		startingViewContainer.addSubview(informationLabel)
+		
+		NSLayoutConstraint.activate([
+			informationLabel.topAnchor.constraint(equalTo: startingViewContainer.topAnchor, constant: 20),
+			informationLabel.leadingAnchor.constraint(equalTo: startingViewContainer.leadingAnchor, constant: 20),
+			informationLabel.trailingAnchor.constraint(equalTo: startingViewContainer.trailingAnchor, constant: -20)
+		])
+	}
+	
+	/// Настройка ограницений (constrains) для startButton
+	private func setupStartButton() {
+		
+		startingViewContainer.addSubview(startButton)
+		
+		NSLayoutConstraint.activate([
+			startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			startButton.bottomAnchor.constraint(equalTo: startingViewContainer.bottomAnchor, constant: -40),
+			startButton.widthAnchor.constraint(equalToConstant: 200),
+			startButton.heightAnchor.constraint(equalToConstant: 50)
+		])
+		
+		startButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+	}
+	
+	// MARK: - Actions
+	@objc func buttonTapped(sender: UIButton) {
+		UIView.animate(
+			withDuration: 0.1,
+			animations: {
+				sender.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+			},
+			completion: { _ in
+				UIView.animate(withDuration: 0.1) {
+					sender.transform = CGAffineTransform.identity
+				}
+			})
+	}
 }
 
