8ab0cacb484dbf59f80ce4e2df0e018a23587b24
DmitryP0l <dmitrypoletaeu@gmail.com>
2024-11-26 21:31:44 +0300

    commit
    Change architecture on ContactListScreen to CleanSwift
    
    add ContactListProtocols

--- SendMoneyApp/ContactListScreen/ContactListProtocols.swift
+++ SendMoneyApp/ContactListScreen/ContactListProtocols.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol ContactListDisplayLogic: AnyObject {
+	func displayUsers(viewModel: ContactList.Users.ViewModel)
+}
+
+protocol ContactListBusinessLogic {
+	func fetchUsers(request: ContactList.Users.Request)
+}
+
+protocol ContactListPresentationLogic {
+	func presentUsers(response: ContactList.Users.Response)
+}
+
+protocol ContactLisrRoutingLogic {
+	func routeToPersonalPage(user: User)
+}
+
+protocol ContactListDataStore {
+	var users: [User]? { get set }
+}
